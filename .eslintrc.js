2cf9e7f65af1aecb700fa7f0dba4b51a9abfa2dd
Krillian <krillian@gmx.de>
2020-07-12 12:28:38 +0200

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'plugin:react-hooks/recommended'],
+  extends: ['airbnb-typescript', 'prettier', 'prettier/@typescript-eslint'],
+};
